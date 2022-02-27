9cc982cb32c5450943efa1cd83363cce324dcd74
Stephanie Mayers <g00286470@stu.raritanval.edu>
2022-02-27 16:19:50 -0500

    Finished

--- src/objects-02.js
+++ src/objects-02.js
@@ -87,7 +87,7 @@ const colorHexCodes = {
  * must use colorHexCodes as a lookup table
  */
 function getColorValue(color) {
-  // write your code here & return value
+  return colorHexCodes[color];
 }
 
 /**
@@ -105,12 +105,14 @@ function getColorValue(color) {
  *
  * book - https://bit.ly/3D5ojn5
  * Mozilla - https://mzl.la/3F6tk0F
- * Or - https://mzl.la/3DnsESS 
+ * Or - https://mzl.la/3DnsESS
  */
 function testForColor(color) {
-  // write your code here & return value
+  if (colorHexCodes.hasOwnProperty(color) === true) {
+    return true;
+  }
+  return false;
 }
-
 module.exports = {
   getColorValue,
   testForColor,
